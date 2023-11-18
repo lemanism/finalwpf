7103fe5b9c628df1ef62149af88c031fba4f111f
Leman <lemanismanli@gmail.com>
2023-11-18 22:40:08 +0400

    salam

---
+++ Database/AllData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Database;
+
+internal class AllData
+{
+    public List<Admin> admins { get; set; }
+    public List<User> users { get; set; }
+    public List<Product> products { get; set; }
+
+}
